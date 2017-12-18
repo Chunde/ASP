0e33190ff369f8546f41109fe65065137e6b51af
Chunde <xmuhcd@msn.com>
2017-12-18 09:14:52 +0800

    model

---
+++ ShoeMarket/Data/ShoeMarketContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ShoeMarket.Models
+{
+    public class ShoeMarketContext : DbContext
+    {
+        public ShoeMarketContext (DbContextOptions<ShoeMarketContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ShoeMarket.Models.Movie> Movie { get; set; }
+    }
+}
